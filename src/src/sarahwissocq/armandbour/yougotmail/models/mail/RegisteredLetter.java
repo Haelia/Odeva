b40c8781edafcc081ed8ee5aa696ef4adee07e83
Haelia <sarahwissocq@free.fr>
2014-11-19 19:14:19 +0100

    test : acknowledgment Classe Registered Letter faite

---
+++ src/src/sarahwissocq/armandbour/yougotmail/models/mail/RegisteredLetter.java
@@ -0,0 +1,26 @@
+package src.sarahwissocq.armandbour.yougotmail.models.mail;
+
+/**
+ * When you send a registered letter, you'll receive an acknowledgment of
+ * receipt. A registered letter cost the price of the letter + 15.
+ * 
+ * @author Armand BOUR
+ * @author Sarah WISSOCQ
+ */
+public class RegisteredLetter extends DecorativeLetter {
+
+	public RegisteredLetter(Letter<?> letter) {
+		super(letter);
+	}
+
+	@Override
+	public void action() {
+		this.receiver.postMail(new AcknowledgmentOfReceipt(this.receiver,
+				this.sender));
+	}
+
+	@Override
+	public float getCost() {
+		return (this.content.getCost() + 15);
+	}
+}
---
+++ src/test/sarahwissocq/armandbour/yougotmail/models/mail/AcknowledgmentOfReceiptTest.java
@@ -0,0 +1,26 @@
+package test.sarahwissocq.armandbour.yougotmail.models.mail;
+
+import static org.junit.Assert.assertEquals;
+import src.sarahwissocq.armandbour.yougotmail.models.City;
+import src.sarahwissocq.armandbour.yougotmail.models.Inhabitant;
+import src.sarahwissocq.armandbour.yougotmail.models.mail.AcknowledgmentOfReceipt;
+import src.sarahwissocq.armandbour.yougotmail.models.mail.Letter;
+import src.sarahwissocq.armandbour.yougotmail.models.mail.Text;
+
+public class AcknowledgmentOfReceiptTest extends LetterTest<Text> {
+
+	@Override
+	protected Letter<Text> createLetter(Inhabitant i, Inhabitant in) {
+		return new AcknowledgmentOfReceipt(i, in);
+	}
+
+	@Override
+	public void getCostTest() {
+		City c = new City("test");
+		Inhabitant send = new Inhabitant("send", c);
+		Inhabitant rece = new Inhabitant("rece", c);
+		Letter<Text> l = new AcknowledgmentOfReceipt(send, rece);
+		assertEquals(0, l.getCost(), 0.1);
+
+	}
+}
